ca2e8c7605f534f5597d3b9ccd5d8d0be9f47a92
Juan Camilo Falla <j.fallag@uniandes.edu.co>
2021-03-11 14:27:42 -0500

    codigo r1

--- App/controller.py
+++ App/controller.py
@@ -23,16 +23,106 @@
 import config as cf
 import model
 import csv
+from datetime import datetime
 
 
 """
 El controlador se encarga de mediar entre la vista y el modelo.
 """
 
-# Inicialización del Catálogo de libros
+def initCatalog():
+    catalog = model.newCatalog()
+    return catalog
 
 # Funciones para la carga de datos
 
+def loadData(catalog):
+    loadCategory(catalog)
+    loadVideos(catalog)
+    
+
+def loadVideos(catalog):
+    """Crea un diccionario con la informacion del video para que sea posteriormente agregado
+    al catalogo en su lista correspondiente"""
+    videosfile = cf.data_dir + 'GoodReads/videos-large.csv'
+    input_file = csv.DictReader(open(videosfile, encoding='utf-8'))
+    for video in input_file:
+        filtrado= {}
+        filtrado["video_id"] = video["video_id"]
+        filtrado["trending_date"] = video["trending_date"]
+        filtrado["title"] = video["title"]
+        filtrado["channel_title"] = video["channel_title"]
+        filtrado["category_id"] = int(video["category_id"])
+        filtrado["publish_time"] = video["publish_time"]
+        filtrado["tags"] = video["tags"]
+        filtrado["country"] = video["country"]
+        filtrado["views"] = int(video["views"])
+        filtrado["likes"] = int(video["likes"])
+        filtrado["dislikes"] = int(video["dislikes"])
+        filtrado['trending_date'] = video['trending_date']
+        filtrado['publish_time'] = video['publish_time']
+        model.addVideo(catalog, filtrado)
+
+def loadCategory(catalog):
+    """Crea un diccionario con la informacion de la categoria
+    para que sea posteriormente agregado
+    al catalogo en su lista correspondiente"""
+    categoriesfile = cf.data_dir + 'GoodReads/category-id.csv'
+    input_file = csv.DictReader(open(categoriesfile, encoding='utf-8'))
+    for category in input_file:
+        category_list = category['id\tname'].split('\t')
+        category['name'] = category_list[1]
+        category['id'] = int(category_list[0])
+        model.addCategory(catalog, category)
+
 # Funciones de ordenamiento
 
+def sortVideos(lista):
+    """Llama a la funcion sortVideos del modelo"""
+    return model.sortVideos(lista)
+
+def sortVideosReq2(lista):
+    """Llama a la funcion sortVideosReq2 del modelo"""
+    return model.sortVideosReq2(lista)
+
+def sortVideosReq3(lista):
+    """Llama a la funcion sortVideosReq3 del modelo"""
+    return model.sortVideosReq3(lista)
+
+def sortDate(lista):
+    return model.sortDate(lista)
+    
+def sortVideosReq4(lista):
+    """Llama a la funcion 'sortVideosReq4()' del modelo"""
+    return model.sortVideosReq4(lista)
+
+def limpieza(lista):
+    """Llama a la funcion 'limpieza()' del modelo"""
+    return model.limpieza(lista)
+
 # Funciones de consulta sobre el catálogo
+
+def filtrado_pais(catalog, pais):
+    """Llama a la funcion 'filtrado_pais()' del modelo"""
+    return model.filtrado_pais(catalog, pais)
+
+def lista(catalog):
+    """Llama a la funcion 'lista()' del modelo"""
+    return model.lista(catalog)
+    
+def filtrado_categoria(lista, categoria):
+    """Llama a la funcion 'filtrado_categoria()' del modelo"""
+    return model.filtrado_categoria(lista, categoria)
+
+def filtrado_tags(catalog, tag):
+    """Llama a la funcion 'filtrado_categoria()' del modelo"""
+    return model.filtrado_tags(catalog, tag)
+
+def idCat(catalog, categoria):
+    return model.idCat(catalog, categoria)
+
+def trending(lista):
+    return model.trending(lista)
+
+def trending_2(lista):
+    return model.trending_2(lista)
